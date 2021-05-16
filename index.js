01e501bff4056949f3509fe7dbaeed0c333e586e
Avraz  Zebary <avrazebary@gmail.com>
2021-05-16 14:42:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+function findMatching(listname, name){
+  return listname.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+// function fuzzyMatch(listname,name){
+  // let lengthOfName = name.length;
+  //   return listname.filter(function (driverName) {
+  //   return driverName.toLowerCase() === name.toLowerCase();
+  // });
+  
+// }
+
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+function matchName(listname,name){
+  return listname.filter(function (driverName) {
+    return driverName.name.toLowerCase() === name.toLowerCase();
+  });
+}
